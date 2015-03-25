9514ed89b10f57a76e0a02f22b7e3713a813a547
James Westby <jw+debian@jameswestby.net>
2015-03-25 16:31:21 +0000

    Get type inference working properly with a standard-ish algo.

--- sylph/typer.py
+++ sylph/typer.py
@@ -6,32 +6,17 @@ class Type(object):
     def __init__(self, name):
         self.name = name
 
-    def __key(self):
-        return (self.__class__, self.name)
-
-    def __eq__(self, other):
-        return self.__key() == other.__key()
-
-    def __ne__(self, other):
-        return not self == other
-
     def __str__(self):
         return self.name
 
     def __repr__(self):
         return "<Type:%s>" % str(self)
 
-    def __hash__(self):
-        return hash(self.__key())
-
 
 class TypeVariable(Type):
 
     def __init__(self, name):
         self.name = name
-
-    def __key(self):
-        return (self.__class__, self.name)
 
     def __str__(self):
         return self.name
@@ -40,30 +25,64 @@ class TypeVariable(Type):
         return "<TypeVariable:%s>" % str(self)
 
 
-class FunctionCallType(Type):
+class FunctionType(Type):
 
-    def __init__(self, name, args):
+    def __init__(self, name, args, rtype):
         self.name = name
         self.args = args
-
-    def __key(self):
-        return (self.__class__, self.name, self.args)
+        self.rtype = rtype
 
     def __str__(self):
         if self.args:
             arg_str = " -> ".join([str(a) for a in self.args])
         else:
             arg_str = "(noargs)"
-        return str(self.name) + " " + arg_str
+        return str(self.name) + "(" + arg_str + ") -> " + str(self.rtype)
 
     def __repr__(self):
         return "<FunctionCall:%s>" % str(self)
+
+
+class ReturnType(TypeVariable):
+
+    def __str__(self):
+        return "return of " + str(self.name)
+
+    def __repr__(self):
+        return "<ReturnType:%s>" % str(self)
+
+
+class Apply(Type):
+
+    def __init__(self, ftype, args):
+        self.ftype = ftype
+        self.args = args
+
+    def __str__(self):
+        return "Application of %s to %s" % (str(self.ftype), str(self.args))
+
+    def __repr__(self):
+        return "<Apply:%s(%s)>" % (str(self.ftype), str(self.args))
 
 
 ANY = Type("ANY")
 INT = Type("int")
 NONE = Type("None")
 BOOL = Type("Boolean")
+
+BASE_TYPES = {
+    "any": ANY,
+    "int": INT,
+    "None": NONE,
+    "bool": BOOL,
+}
+
+
+def type_from_decl(type_str):
+    try:
+        return BASE_TYPES[type_str]
+    except KeyError:
+        raise AssertionError("Unknown type: %s" % type_str)
 
 
 def create_typevars(arg, sourcepos):
@@ -114,7 +133,7 @@ class TypeCollector(ASTVisitor):
 
     def visit_Conditional(self, node):
         condition, true_block, false_block = node.children
-        self.equalities.append((BOOL, self.dispatch(condition), [condition.sourcepos]))
+        self.equalities.append((self.dispatch(condition), BOOL, [condition.sourcepos]))
         self.dispatch(true_block)
         if false_block is not None:
             self.dispatch(false_block)
@@ -122,62 +141,53 @@ class TypeCollector(ASTVisitor):
 
     def visit_While(self, node):
         condition, block = node.children
-        self.equalities.append((BOOL, self.dispatch(condition), [condition.sourcepos]))
+        self.equalities.append((self.dispatch(condition), BOOL, [condition.sourcepos]))
         self.dispatch(block)
         return None
 
-    def visit_BinOp(self, node):
+    def _handle_function(self, node, name):
         args = [self.dispatch(c) for c in node.children]
-        new_args = []
-        for arg in args:
-            new_arg, new_eqs = create_typevars(arg, node.sourcepos)
-            self.equalities.extend(new_eqs)
-            new_args.append(new_arg)
-        return FunctionCallType(node.op, new_args)
+        ftype = self.get_typevar(name)
+        return ReturnType(Apply(ftype, args))
+
+    def visit_BinOp(self, node):
+        return self._handle_function(node, node.op)
 
     def visit_Function(self, node):
-        args = [self.dispatch(c) for c in node.children]
-        ftype = self.varmap.setdefault(node.fname, TypeVariable(node.fname))
-        new_args = []
-        for arg in args:
-            new_arg, new_eqs = create_typevars(arg, node.sourcepos)
-            self.equalities.extend(new_eqs)
-            new_args.append(new_arg)
-        call = FunctionCallType(ftype, new_args)
-        rtype = TypeVariable("return of " + str(call))
-        self.equalities.append((rtype, call, [node.sourcepos]))
-        return rtype
+        return self._handle_function(node, node.fname)
 
     def visit_FuncDef(self, node):
         child = TypeCollector()
         # XXX: catch redfinition of functions
         self.child_contexts[node.name] = child
         if node.rtype:
-            child.rtype = Type(node.rtype)
+            child.rtype = type_from_decl(node.rtype)
         for i, argtype_str in enumerate(node.argtypes):
             argtype = TypeVariable(node.args[i])
             if argtype_str is not None:
                 # XXX: should look up type based on name
-                argtype = Type(argtype_str)
+                argtype = type_from_decl(argtype_str)
             child.varmap[node.args[i]] = argtype
+        ftype = self.get_typevar(node.name)
+        child.varmap[node.name] = ftype
+        self.varmap[node.name] = ftype
         child.args = node.args
         child.dispatch(node.children[0])
+        # XXX: catch function redefinition/shadowing?
         return None
 
     def general_nonterminal_visit(self, node):
         [self.dispatch(c) for c in node.children]
 
 
-# XXX: These need to match the functions that are in scope
-# at the time
 FUNCTIONS = {
-    '+': (INT, INT, INT),
-    '-': (INT, INT, INT),
-    '*': (INT, INT, INT),
-    '>': (INT, INT, BOOL),
-    '==': (INT, INT, BOOL),
-    'true': (BOOL,),
-    'print': (INT, NONE),
+    '+': FunctionType("+", [INT, INT], INT),
+    '-': FunctionType("-", [INT, INT], INT),
+    '*': FunctionType("*", [INT, INT], INT),
+    '>': FunctionType(">", [INT, INT], BOOL),
+    '==': FunctionType("==", [INT, INT], BOOL),
+    'true': FunctionType("true", [], BOOL), 
+    'print': FunctionType("print", [ANY], NONE), 
 }
 
 
@@ -219,94 +229,146 @@ class SylphTypeError(Exception):
         return "\n".join(lines)
 
 
-def find_function(rtype, ftype, varmap, positions, functions):
-    if isinstance(ftype.name, TypeVariable):
-        if not ftype.name in varmap:
-            raise SylphNameError("%s used before assignment" % ftype.name, positions)
-        name = varmap[ftype.name].name
-        candidate = functions.get(name, [])
-    else:
-        candidate = functions.get(ftype.name, [])
-    argtypes = ()
-    for arg in ftype.args:
-        if isinstance(arg, TypeVariable):
-            if arg in varmap:
-                argtypes += (varmap[arg],)
-            else:
-                raise SylphNameError("%s used before assignment" % arg, positions)
-        else:
-            argtypes += (arg,)
-    if candidate[:-1] == argtypes:
-        if rtype is None or candidate[-1] == rtype:
-            return candidate
-        else:
-            raise SylphTypeError("Function %s over %s returns %s not %s" % (ftype.name, " -> ".join([str(a) for a in argtypes]), candidate[-1], rtype), positions)
-    else:
-        raise SylphTypeError("Can't find function for %s with signature (%s)" % (ftype.name, " -> ".join([str(a) for a in argtypes])), positions)
+def unify_types(a, b):
+    if a is ANY or b is ANY:
+        return ANY
+    if a == b:
+        return a
 
 
-def unify_types(a, b, varmap, positions, functions):
-    if isinstance(b, FunctionCallType):
-        find_function(a, b, varmap, positions, functions)
-    else:
-        if a != b:
-            raise SylphTypeError("%s != %s" % (a, b), positions)
+def occurs(lhs, rhs):
+    if isinstance(rhs, ReturnType):
+        if lhs == rhs:
+            return True
+        if occurs(lhs, rhs.name):
+            return True
+    elif isinstance(rhs, TypeVariable):
+        pass
+    elif isinstance(rhs, FunctionType):
+        if lhs == rhs:
+            return True
+        for arg in rhs.args:
+            if occurs(lhs, arg):
+                return True
+        if occurs(lhs, rhs.rtype):
+            return True
+    elif isinstance(rhs, Apply):
+        if lhs == rhs:
+            return True
+        if occurs(lhs, rhs.ftype):
+            return True
+        for arg in rhs.args:
+            if occurs(lhs, arg):
+                return True
+
+
+def update_substitution(substition, lhs, rhs, positions):
+    if occurs(lhs, rhs):
+        raise SylphTypeError("Recursive type definition: %s = %s" % (lhs, rhs), positions)
+    for other_lhs, candidate in substition.items():
+        # TODO: recurse in to function types in rhs?
+        if candidate == lhs:
+            substition[other_lhs] = rhs
+        if other_lhs == lhs:
+            substition[rhs] = candidate
+    substition[lhs] = rhs
+
+
+def handle_return_type(rtype, required_rtype, equalities, functions, substition, positions):
+    tapply = rtype.name
+    ftype = tapply.ftype
+    defined = ftype
+    if defined in substition:
+        defined = substition[ftype]
+    if not isinstance(defined, FunctionType):
+        if defined.name not in functions:
+            raise SylphNameError("Unknown function: %s" % defined.name, positions)
+        defined = functions.get(defined.name)
+    if not isinstance(defined, FunctionType):
+        raise SylphTypeError("Attempted to call a non-function %s" % (defined.name), positions)
+    if len(defined.args) != len(tapply.args):
+        raise SylphTypeError("Incorrect number of args for function %s, expected %d, got %d" % (defined.name, len(defined.args), len(tapply.args)), positions)
+    for arg1, arg2 in zip(defined.args, tapply.args):
+        equalities.insert(0, (arg2, arg1, positions))
+    equalities.insert(0, (required_rtype, defined.rtype, positions))
 
 
 def satisfy_equalities(equalities, varmap, functions):
-    for equality in equalities:
+    substition = dict()
+    while equalities:
+        equality = equalities.pop(0)
         lhs, rhs, positions = equality
         if isinstance(lhs, TypeVariable):
-            if isinstance(rhs, TypeVariable):
-                if rhs in varmap:
-                    if lhs in varmap:
-                        unify_types(varmap[lhs], rhs, varmap, positions, functions)
-                    else:
-                        varmap[lhs] = varmap[rhs]
-                else:
-                    raise SylphNameError("%s used before assignment" % rhs.name, positions)
-            elif isinstance(rhs, FunctionCallType):
-                if lhs in varmap:
-                    find_function(varmap[lhs], rhs, varmap, positions, functions)
-                else:
-                    ftype = find_function(None, rhs, varmap, positions, functions)
-                    varmap[lhs] = ftype[-1]
+            if lhs in substition:
+                # TODO: store positions in the substition and update
+                # the list as things are replaced to get full list of
+                # involved lines?
+                equalities.insert(0, (substition[lhs], rhs, positions))
+                continue
             else:
-                if lhs in varmap:
-                    unify_types(varmap[lhs], rhs, varmap, positions, functions)
-                varmap[lhs] = rhs
+                if isinstance(lhs, ReturnType):
+                    handle_return_type(lhs, rhs, equalities, functions, substition, positions)
+                elif isinstance(rhs, ReturnType):
+                    handle_return_type(rhs, lhs, equalities, functions, substition, positions)
+                if lhs != rhs:
+                    update_substitution(substition, lhs, rhs, positions)
+        elif isinstance(lhs, FunctionType):
+            if not isinstance(rhs, FunctionType):
+                raise SylphTypeError("Types don't match %s != %s" % (lhs, rhs), positions)
+            if len(lhs.args) != len(rhs.args):
+                raise SylphTypeError("Types don't match %s != %s, argument lengths differ" % (lhs, rhs), positions)
+            for i, arg in enumerate(lhs.args):
+                equalities.insert(0, (arg, rhs.args[i], positions))
         else:
-            unify_types(lhs, rhs, varmap, positions, functions)
+            if isinstance(rhs, TypeVariable):
+                if rhs in substition:
+                    equalities.insert(0, (lhs, substition[rhs], positions))
+                    continue
+                else:
+                    if isinstance(rhs, ReturnType):
+                        handle_return_type(rhs, lhs, equalities, functions, substition, positions)
+                        continue
+            if not unify_types(lhs, rhs):
+                raise SylphTypeError("Types don't match %s != %s" % (lhs, rhs), positions)
+    return substition
+
+
+def functions_from_vars(varmap):
+    functions = {}
+    for name, t in varmap.items():
+        if isinstance(t, FunctionType):
+            functions[TypeVariable(name)] = t
+    return functions
+
+
+def function_type_from_context(t, prefix_names=True, substitions=None):
+    if substitions is None:
+        substitions = {}
+    def get_substituted(var):
+        while var in substitions:
+            var = substitions[var]
+        if prefix_names and isinstance(var, TypeVariable):
+            var = TypeVariable(t.fname + ":" + var.name)
+        return var
+    rtype = get_substituted(t.rtype)
+    args = [get_substituted(t.varmap[arg]) for arg in t.args]
+    return FunctionType(t.fname, args, rtype)
 
 
 def _typecheck(t):
     functions = FUNCTIONS.copy()
+    if t.fname:
+        functions[t.fname] = function_type_from_context(t, prefix_names=False)
     varmap = {}
-    varmap[TypeVariable('print')] = TypeVariable('print')
-    if t.fname is not None:
-        args = ()
-        for arg in t.args:
-            args += (t.varmap[arg],)
-        args += (t.rtype,)
-        functions[t.fname] = args
-        varmap[TypeVariable(t.fname)] = TypeVariable(t.fname)
-        #varmap[t.fname] = FunctionCallType(TypeVariable(t.fname), args)
     for name, context in t.child_contexts.items():
-        context.varmap[TypeVariable(name)] = FunctionCallType(TypeVariable(name), [])
         context.fname = name
-        _typecheck(context)
-        args = ()
-        for arg in context.args:
-            args += (context.varmap[arg],)
-        functions[name] = args + (context.rtype,)
-        varmap[TypeVariable(name)] = FunctionCallType(name, [])
-    satisfy_equalities(t.equalities, varmap, functions)
+        child_subs = _typecheck(context)
+        functions[name] = function_type_from_context(context, substitions=child_subs)
+    functions.update(functions_from_vars(t.varmap))
+    return satisfy_equalities(t.equalities, varmap, functions)
 
 
 def typecheck(node):
-    names = GatherNames().dispatch(node)
     t = TypeCollector()
-    for name in names:
-        t.varmap[name] = TypeVariable(name)
     t.dispatch(node)
     _typecheck(t)
