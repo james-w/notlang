1b6d61125a64507df2550eb135c777d628e1c3da
James Westby <jw+debian@jameswestby.net>
2015-03-25 22:05:32 +0000

    Get the tests passing again.

--- sylph/testing.py
+++ sylph/testing.py
@@ -1,6 +1,6 @@
-from testtools.matchers import Equals
+from testtools.matchers import Equals, IsInstance, MatchesAll, AfterPreprocessing, MatchesListwise
 
-from . import bytecode
+from . import bytecode, typer
 
 
 class BytecodeMatches(object):
@@ -19,3 +19,35 @@ class BytecodeMatches(object):
 
     def match(self, actual):
         return self.matcher.match(map(self.name, self.pair_up(map(ord, actual))))
+
+
+class IsTypeExpr(object):
+
+    def __init__(self, name):
+        self.matcher = MatchesAll(
+            IsInstance(typer.TypeExpr),
+            AfterPreprocessing(lambda a: a.name, Equals(name))
+        )
+
+    def __str__(self):
+        return str(self.matcher)
+
+    def match(self, actual):
+        return self.matcher.match(actual)
+
+
+class IsFunctionType(object):
+
+    def __init__(self, name, args, rtype):
+        self.matcher = MatchesAll(
+            IsInstance(typer.FunctionType),
+            AfterPreprocessing(lambda a: a.name, name),
+            AfterPreprocessing(lambda a: a.args, MatchesListwise(args)),
+            AfterPreprocessing(lambda a: a.rtype, rtype),
+        )
+
+    def __str__(self):
+        return str(self.matcher)
+
+    def match(self, actual):
+        return self.matcher.match(actual)
--- sylph/tests/test_typer.py
+++ sylph/tests/test_typer.py
@@ -1,7 +1,8 @@
 from testtools import TestCase
+from testtools.matchers import Equals, Is
 from rpython.rlib.parsing.lexer import SourcePos
 
-from .. import ast, typer
+from .. import ast, testing, typer
 
 
 class TypeCollectorTests(TestCase):
@@ -13,7 +14,7 @@ class TypeCollectorTests(TestCase):
         t = typer.TypeCollector()
         rtype = t.dispatch(node)
         self.assertIs(typer.INT, rtype)
-        self.assertEqual([], t.equalities)
+        self.assertEqual([], t.constraints)
 
     def test_Assignment(self):
         varname = "a"
@@ -22,11 +23,10 @@ class TypeCollectorTests(TestCase):
         node = ast.Assignment(lhs, rhs, self.spos)
         t = typer.TypeCollector()
         rtype = t.dispatch(node)
-        vartype = typer.TypeVariable(varname)
-        self.assertEqual(vartype, rtype)
-        self.assertEqual(1, len(t.equalities))
-        self.assertEqual((vartype, typer.INT, [self.spos]), t.equalities[0])
-        self.assertEqual(vartype, t.varmap[varname])
+        self.assertThat(rtype, testing.IsTypeExpr(varname))
+        self.assertEqual(1, len(t.constraints))
+        self.assertEqual((rtype, typer.SUPERTYPE_OF, typer.INT, [self.spos]), t.constraints[0])
+        self.assertEqual(rtype, t.varmap[varname])
 
     def test_Return(self):
         rhs = ast.ConstantInt(2, self.spos)
@@ -34,16 +34,16 @@ class TypeCollectorTests(TestCase):
         t = typer.TypeCollector()
         rtype = t.dispatch(node)
         self.assertIs(None, rtype)
-        self.assertEqual(1, len(t.equalities))
-        self.assertEqual((t.rtype, typer.INT, [self.spos]), t.equalities[0])
+        self.assertEqual(1, len(t.constraints))
+        self.assertEqual((t.rtype, typer.SUPERTYPE_OF, typer.INT, [self.spos]), t.constraints[0])
 
     def test_Return_noarg(self):
         node = ast.Return(None, self.spos)
         t = typer.TypeCollector()
         rtype = t.dispatch(node)
         self.assertIs(None, rtype)
-        self.assertEqual(1, len(t.equalities))
-        self.assertEqual((t.rtype, typer.NONE, [self.spos]), t.equalities[0])
+        self.assertEqual(1, len(t.constraints))
+        self.assertEqual((t.rtype, typer.SUPERTYPE_OF, typer.NONE, [self.spos]), t.constraints[0])
 
     def test_Variable_existing(self):
         varname = "a"
@@ -52,42 +52,52 @@ class TypeCollectorTests(TestCase):
         vartype = typer.TypeVariable(varname)
         t.varmap[varname] = vartype
         self.assertIs(vartype, t.dispatch(node))
-        self.assertEqual([], t.equalities)
+        self.assertEqual([], t.constraints)
 
     def test_Variable_nonexisting(self):
         varname = "a"
         node = ast.Variable(varname, self.spos)
         t = typer.TypeCollector()
-        self.assertEquals(typer.TypeVariable(varname), t.dispatch(node))
-        self.assertEqual([], t.equalities)
+        self.assertRaises(typer.SylphNameError, t.dispatch, node)
 
     def test_BinOp(self):
         varname = "a"
         op = "+"
         lhs = ast.ConstantInt(1, self.spos)
-        rhs = ast.Variable(varname, self.spos)
-        node = ast.BinOp(op, lhs, rhs, self.spos)
+        node = ast.BinOp(op, lhs, lhs, self.spos)
         t = typer.TypeCollector()
         rtype = t.dispatch(node)
-        self.assertEqual(typer.FunctionCallType(op, [typer.INT, typer.TypeVariable(varname)]), rtype)
-        self.assertEqual([], t.equalities)
+        self.assertIsInstance(rtype, typer.TypeExpr)
+        self.assertEqual("r+", rtype.name)
+        self.assertEqual(1, len(t.constraints))
+        constraint = t.constraints[0]
+        self.assertThat(constraint[0], testing.IsTypeExpr("+"))
+        self.assertEqual(constraint[1], typer.SUPERTYPE_OF)
+        self.assertThat(constraint[2], testing.IsFunctionType(testing.IsTypeExpr("+"), [Equals(typer.INT), Equals(typer.INT)], testing.IsTypeExpr("r+")))
+        self.assertEqual([self.spos], constraint[3])
 
     def test_nested_BinOp(self):
         varname = "a"
         op = "+"
         lhs = ast.ConstantInt(1, self.spos)
-        rhs = ast.Variable(varname, self.spos)
-        node = ast.BinOp(op, lhs, rhs, self.spos)
+        node = ast.BinOp(op, lhs, lhs, self.spos)
         node = ast.BinOp(op, lhs, node, self.spos)
         t = typer.TypeCollector()
         rtype = t.dispatch(node)
         new_tvar = typer.TypeVariable("(rtype of + int -> a)")
-        self.assertEqual(typer.FunctionCallType(op, [typer.INT, new_tvar]), rtype)
-        self.assertEqual(
-            [(new_tvar,
-              typer.FunctionCallType(op, [typer.INT, typer.TypeVariable(varname)]),
-              [self.spos])],
-            t.equalities)
+        self.assertIsInstance(rtype, typer.TypeExpr)
+        self.assertEqual("r+", rtype.name)
+        self.assertEqual(2, len(t.constraints))
+        constraint = t.constraints[0]
+        self.assertThat(constraint[0], testing.IsTypeExpr("+"))
+        self.assertEqual(constraint[1], typer.SUPERTYPE_OF)
+        self.assertThat(constraint[2], testing.IsFunctionType(testing.IsTypeExpr("+"), [Equals(typer.INT), Equals(typer.INT)], testing.IsTypeExpr("r+")))
+        self.assertEqual([self.spos], constraint[3])
+        constraint = t.constraints[1]
+        self.assertThat(constraint[0], testing.IsTypeExpr("+"))
+        self.assertEqual(constraint[1], typer.SUPERTYPE_OF)
+        self.assertThat(constraint[2], testing.IsFunctionType(testing.IsTypeExpr("+"), [Equals(typer.INT), testing.IsTypeExpr("r+")], testing.IsTypeExpr("r+")))
+        self.assertEqual([self.spos], constraint[3])
         # probably applies to foo(bar(baz)) too?
         # also test foo(bar) + 1 etc.
         # Maybe need to create a lot more typevars and add equalities?
@@ -100,9 +110,10 @@ class TypeCollectorTests(TestCase):
         t = typer.TypeCollector()
         rtype = t.dispatch(node)
         self.assertIs(None, rtype)
-        self.assertEqual(2, len(t.equalities))
-        self.assertEqual((typer.BOOL, typer.INT, [self.spos]), t.equalities[0])
-        self.assertEqual((typer.TypeVariable(varname), typer.INT, [self.spos]), t.equalities[1])
+        self.assertEqual(2, len(t.constraints))
+        self.assertEqual((typer.INT, typer.SUBTYPE_OF, typer.BOOL, [self.spos]), t.constraints[0])
+        self.assertEqual((typer.SUPERTYPE_OF, typer.INT, [self.spos]), t.constraints[1][1:])
+        self.assertThat(t.constraints[1][0], testing.IsTypeExpr(varname))
 
     def test_Conditional_with_else(self):
         varname = "a"
@@ -112,10 +123,12 @@ class TypeCollectorTests(TestCase):
         t = typer.TypeCollector()
         rtype = t.dispatch(node)
         self.assertIs(None, rtype)
-        self.assertEqual(3, len(t.equalities))
-        self.assertEqual((typer.BOOL, typer.INT, [self.spos]), t.equalities[0])
-        self.assertEqual((typer.TypeVariable(varname), typer.INT, [self.spos]), t.equalities[1])
-        self.assertEqual((typer.TypeVariable(varname), typer.INT, [self.spos]), t.equalities[2])
+        self.assertEqual(3, len(t.constraints))
+        self.assertEqual((typer.INT, typer.SUBTYPE_OF, typer.BOOL, [self.spos]), t.constraints[0])
+        self.assertEqual((typer.SUPERTYPE_OF, typer.INT, [self.spos]), t.constraints[1][1:])
+        self.assertThat(t.constraints[1][0], testing.IsTypeExpr(varname))
+        self.assertEqual((typer.SUPERTYPE_OF, typer.INT, [self.spos]), t.constraints[2][1:])
+        self.assertThat(t.constraints[1][0], testing.IsTypeExpr(varname))
 
     def test_While(self):
         varname = "a"
@@ -125,18 +138,24 @@ class TypeCollectorTests(TestCase):
         t = typer.TypeCollector()
         rtype = t.dispatch(node)
         self.assertIs(None, rtype)
-        self.assertEqual(2, len(t.equalities))
-        self.assertEqual((typer.BOOL, typer.INT, [self.spos]), t.equalities[0])
-        self.assertEqual((typer.TypeVariable(varname), typer.INT, [self.spos]), t.equalities[1])
+        self.assertEqual(2, len(t.constraints))
+        constraint = t.constraints[0]
+        self.assertEqual((typer.INT, typer.SUBTYPE_OF, typer.BOOL, [self.spos]), constraint)
+        constraint = t.constraints[1]
+        self.assertEqual((typer.SUPERTYPE_OF, typer.INT, [self.spos]), constraint[1:])
+        self.assertThat(constraint[0], testing.IsTypeExpr(varname))
 
     def test_Function_noargs(self):
         fname = "foo"
         node = ast.Function(fname, [], self.spos)
         t = typer.TypeCollector()
         rtype = t.dispatch(node)
-        self.assertEqual(typer.TypeVariable("return of %s (noargs)" % fname), rtype)
-        
-        self.assertEqual([(rtype, typer.FunctionCallType(typer.TypeVariable(fname), []), [self.spos])], t.equalities)
+        self.assertThat(rtype, testing.IsTypeExpr("rfoo"))
+        constraint = t.constraints[0]
+        self.assertThat(constraint[0], testing.IsTypeExpr(fname))
+        self.assertEqual(typer.SUPERTYPE_OF, constraint[1])
+        self.assertThat(constraint[2], testing.IsFunctionType(testing.IsTypeExpr(fname), [], Is(rtype)))
+        self.assertEqual([self.spos], constraint[3])
 
     # TODO: test recursion
 
@@ -146,10 +165,12 @@ class TypeCollectorTests(TestCase):
         node = ast.Function(fname, [arg], self.spos)
         t = typer.TypeCollector()
         rtype = t.dispatch(node)
-        self.assertEqual(typer.TypeVariable("return of %s int" % fname), rtype)
-        self.assertEqual(
-            [(rtype, typer.FunctionCallType(typer.TypeVariable(fname), [typer.INT]), [self.spos])],
-            t.equalities)
+        self.assertThat(rtype, testing.IsTypeExpr("rfoo"))
+        constraint = t.constraints[0]
+        self.assertThat(constraint[0], testing.IsTypeExpr(fname))
+        self.assertEqual(typer.SUPERTYPE_OF, constraint[1])
+        self.assertThat(constraint[2], testing.IsFunctionType(testing.IsTypeExpr(fname), [Is(typer.INT)], Is(rtype)))
+        self.assertEqual([self.spos], constraint[3])
 
     def test_FuncDef(self):
         fname = "foo"
@@ -159,32 +180,33 @@ class TypeCollectorTests(TestCase):
         t = typer.TypeCollector()
         rtype = t.dispatch(node)
         self.assertIs(None, rtype)
-        self.assertEqual([], t.equalities)
+        self.assertEqual([], t.constraints)
         self.assertEqual(1, len(t.child_contexts))
         self.assertIn(fname, t.child_contexts)
         context = t.child_contexts[fname]
         self.assertIsInstance(context, typer.TypeCollector)
-        self.assertEqual([(context.rtype, typer.TypeVariable(argname), [self.spos])], context.equalities)
-        self.assertEqual({argname: typer.TypeVariable(argname)}, context.varmap)
+        self.assertEqual([(context.rtype, typer.SUPERTYPE_OF, context.varmap[argname], [self.spos])], context.constraints)
+        self.assertEqual(2, len(context.varmap))
+        self.assertThat(context.varmap[argname], testing.IsTypeExpr(argname))
+        self.assertThat(context.varmap[fname], testing.IsTypeExpr(fname))
 
     def test_FuncDef_with_annotations(self):
         fname = "foo"
         argname = "bar"
-        rtype = "Boolean"
+        rtype = "bool"
         argtype = "int"
         code = ast.Return(ast.Variable(argname, self.spos), self.spos)
         node = ast.FuncDef(fname, [argname], code, self.spos, rtype=rtype, argtypes=[argtype])
         t = typer.TypeCollector()
         self.assertIs(None, t.dispatch(node))
-        self.assertEqual([], t.equalities)
+        self.assertEqual([], t.constraints)
         self.assertEqual(1, len(t.child_contexts))
         self.assertIn(fname, t.child_contexts)
         context = t.child_contexts[fname]
         self.assertIsInstance(context, typer.TypeCollector)
-        # XXX: check that these are actually BOOL and INT
-        self.assertEqual([(context.rtype, typer.Type(argtype), [self.spos])], context.equalities)
-        self.assertEqual(typer.Type(rtype), context.rtype)
-        self.assertEqual(typer.Type(argtype), context.varmap[argname])
+        self.assertEqual([(typer.BOOL, typer.SUPERTYPE_OF, typer.INT, [self.spos])], context.constraints)
+        self.assertEqual(typer.BOOL, context.rtype)
+        self.assertEqual(typer.INT, context.varmap[argname])
 
     def test_Block(self):
         # Test that children are dispatched to by default
@@ -194,106 +216,36 @@ class TypeCollectorTests(TestCase):
         node = ast.Assignment(lhs, rhs, self.spos)
         t = typer.TypeCollector()
         t.dispatch(ast.Block([node], self.spos))
-        self.assertEqual(1, len(t.equalities))
+        self.assertEqual(1, len(t.constraints))
 
 
-class SatisfyEqualitiesTests(TestCase):
+class SatisfyConstraintsTests(TestCase):
 
     spos = SourcePos(0, 0, 0)
 
-    def satisfy_equalities(self, equalities):
-        varmap = {}
-        functions = typer.FUNCTIONS
-        return typer.satisfy_equalities(equalities, varmap, functions)
+    def satisfy_constraints(self, constraints):
+        return typer.satisfy_constraints(constraints)
 
     def test_empty(self):
-        self.satisfy_equalities([])
+        self.satisfy_constraints([])
 
     def test_trivially_equal(self):
-        self.satisfy_equalities([(typer.INT, typer.INT, [self.spos])])
+        self.satisfy_constraints([(typer.INT, typer.SUPERTYPE_OF, typer.INT, [self.spos])])
 
     def test_trivially_not_equal(self):
-        self.assertRaises(typer.SylphTypeError, self.satisfy_equalities,
-            [(typer.NONE, typer.INT, [self.spos])])
+        self.assertRaises(typer.SylphTypeError, self.satisfy_constraints,
+            [(typer.NONE, typer.SUPERTYPE_OF, typer.INT, [self.spos])])
 
     def test_not_equal_across_two(self):
         vartype = typer.TypeVariable("a")
-        self.assertRaises(typer.SylphTypeError, self.satisfy_equalities,
-            [(vartype, typer.INT, [self.spos]),
-             (vartype, typer.NONE, [self.spos])])
-
-    def test_equal_across_two(self):
-        vartype = typer.TypeVariable("a")
-        self.satisfy_equalities(
-            [(vartype, typer.INT, [self.spos]),
-             (vartype, typer.INT, [self.spos])])
+        self.assertRaises(typer.SylphTypeError, self.satisfy_constraints,
+            [(vartype, typer.SUPERTYPE_OF, typer.INT, [self.spos]),
+             (vartype, typer.SUPERTYPE_OF, typer.NONE, [self.spos])])
 
     def test_not_equal_across_three(self):
         vartype1 = typer.TypeVariable("a")
         vartype2 = typer.TypeVariable("b")
-        self.assertRaises(typer.SylphTypeError, self.satisfy_equalities,
-            [(vartype1, typer.INT, [self.spos]),
-             (vartype2, vartype1, [self.spos]),
-             (vartype2, typer.NONE, [self.spos])])
-
-    def test_equal_across_three(self):
-        vartype1 = typer.TypeVariable("a")
-        vartype2 = typer.TypeVariable("b")
-        self.satisfy_equalities(
-            [(vartype1, typer.INT, [self.spos]),
-             (vartype2, vartype1, [self.spos]),
-             (vartype2, typer.INT, [self.spos])])
-
-    def test_use_before_assignment(self):
-        vartype1 = typer.TypeVariable("a")
-        vartype2 = typer.TypeVariable("b")
-        self.assertRaises(typer.SylphNameError, self.satisfy_equalities,
-            [(vartype1, typer.INT, [self.spos]),
-             (vartype1, vartype2, [self.spos]),
-             (vartype2, typer.NONE, [self.spos])])
-
-    def test_function(self):
-        self.satisfy_equalities(
-            [(typer.INT, typer.FunctionCallType('+', [typer.INT, typer.INT]), [self.spos])])
-
-    def test_no_function(self):
-        self.assertRaises(typer.SylphTypeError,
-            self.satisfy_equalities,
-            [(typer.NONE,
-              typer.FunctionCallType('+', [typer.INT, typer.INT]),
-              [self.spos])])
-
-    def test_function_with_typevar_arg(self):
-        vartype = typer.TypeVariable("a")
-        self.satisfy_equalities(
-            [(vartype, typer.INT, [self.spos]),
-             (typer.INT, typer.FunctionCallType('+', [vartype, typer.INT]), [self.spos])])
-
-    def test_function_with_undefined_typevar_arg(self):
-        vartype = typer.TypeVariable("a")
-        self.assertRaises(typer.SylphNameError,
-            self.satisfy_equalities,
-            [(typer.INT, typer.FunctionCallType('+', [vartype, typer.INT]), [self.spos])])
-
-    def test_function_with_noargs(self):
-        self.satisfy_equalities(
-            [(typer.BOOL, typer.FunctionCallType('true', []), [self.spos])])
-
-    def test_equal_to_multiple_functions(self):
-        vartype = typer.TypeVariable("a")
-        self.satisfy_equalities(
-            [(vartype, typer.INT, [self.spos]),
-             (vartype, typer.FunctionCallType('+', [vartype, typer.INT]), [self.spos]),
-             (vartype, typer.FunctionCallType('-', [vartype, typer.INT]), [self.spos])])
-
-    def test_no_matching_function_with_typevar(self):
-        vartype = typer.TypeVariable("a")
-        self.assertRaises(typer.SylphTypeError, self.satisfy_equalities,
-            [(vartype, typer.BOOL, [self.spos]),
-             (vartype, typer.FunctionCallType('+', [vartype, typer.INT]), [self.spos])])
-
-    def test_function_with_alias(self):
-        vartype = typer.TypeVariable("a")
-        self.assertRaises(typer.SylphTypeError, self.satisfy_equalities,
-            [(vartype, typer.BOOL, [self.spos]),
-             (vartype, typer.FunctionCallType('+', [vartype, typer.INT]), [self.spos])])
+        self.assertRaises(typer.SylphTypeError, self.satisfy_constraints,
+            [(vartype1, typer.SUPERTYPE_OF, typer.INT, [self.spos]),
+             (vartype2, typer.SUPERTYPE_OF, vartype1, [self.spos]),
+             (vartype2, typer.SUPERTYPE_OF, typer.NONE, [self.spos])])
